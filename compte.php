f334429cae12bfc5265771eed60a5061fd8efd67
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2021-11-02 10:36:30 -0400

    Formulaires de création de compte et de réinitialisation du MdP ajoutés.

--- compte.php
+++ compte.php
@@ -3,25 +3,54 @@
     include('inclusions/entete.php');
 ?>
     <section class="principale">
-    <?php if(!isset($_SESSION['uc'])) {  ?>
-        <form action="compte.php" method="post">
-            <fieldset>
-                <legend><?= $frmLegende; ?></legend>
-                <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
-                <input type="password" name="mdp" id="mdp" placeholder="<?= $frmMdpPH; ?>">
-                <input type="submit" value="<?= $frmBoutonConnecter; ?>">
-                <?php if(isset($erreurConnexion)) : ?>
-                    <div class="erreur">Erreur de connexion : réessayez!</div>
-                <?php endif; ?>
-            </fieldset>
-            <div class="actions-compte">
-                <a href="#"><?= $lienMdpOublie; ?></a>
-                <a href="#"><?= $lienNouveauCompte; ?></a>
-            </div>
-        </form>
-    <?php } else { ?>
+    <?php if(!isset($_SESSION['uc'])) :  ?>
+        <div class="formulaires-utilisateurs">
+            <!-- Connexion -->
+            <form action="compte.php" method="post">
+                <fieldset>
+                    <legend><?= $frmLegende; ?></legend>
+                    <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
+                    <input type="password" name="mdp" placeholder="<?= $frmMdpPH; ?>">
+                    <input type="submit" value="<?= $frmBoutonConnecter; ?>">
+                </fieldset>
+                <div class="actions-compte">
+                    <a href="#"><?= $lienMdpOublie; ?></a>
+                    <a href="#"><?= $lienNouveauCompte; ?></a>
+                </div>
+            </form>
+            <!-- Nouveau compte -->
+            <form action="compte.php" method="post">
+                <fieldset>
+                    <legend>Créer un nouveau compte</legend>
+                    <input type="text" name="prenom" placeholder="Prénom">
+                    <input type="text" name="nom" placeholder="Nom">
+                    <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
+                    <input type="password" name="mdp" placeholder="<?= $frmMdpPH; ?>">
+                    <input type="submit" value="Soumettre">
+                </fieldset>
+                <div class="actions-compte">
+                    <a href="#">Connexion</a>
+                </div>
+            </form>
+            <!-- Mot de passe oublié -->
+            <form action="compte.php" method="post">
+                <fieldset>
+                    <legend>Réinitialiser votre mot de passe</legend>
+                    <input type="text" name="courriel" placeholder="<?= $frmCourrielPH; ?>">
+                    <input type="submit" value="Soumettre">
+                </fieldset>
+                <div class="actions-compte">
+                    <a href="#">Connexion</a>
+                    <a href="#"><?= $lienNouveauCompte; ?></a>
+                </div>
+            </form>
+            <?php if(isset($erreurConnexion)) : ?>
+                <div class="erreur">Erreur de connexion : réessayez!</div>
+            <?php endif; ?>
+        </div>
+    <?php else : ?>
         <h2>Mes investissements</h2>
-    <?php } ?>
+    <?php endif; ?>
     </section>
 <?php
     include('inclusions/pied2page.php')
